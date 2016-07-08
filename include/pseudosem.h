ce6e765b23751bee6e2aa6620e201d4f58d37efd
Oliver Hamlet <oliver.hamlet@gmail.com>
2016-07-08 06:53:28 +0100

    Refactor to be more object-oriented

--- include/pseudosem.h
+++ include/pseudosem.h
@@ -8,89 +8,187 @@
 
 namespace pseudosem {
     namespace detail {
-        // Trim start and end of a string, removing any of the given characters.
-        void trim(std::string& str, const std::string& toTrim) {
-            size_t pos = str.find_first_not_of(toTrim);
-            if (pos != 0)
-                str.erase(0, str.find_first_not_of(toTrim));
+        struct VersionParts {
+            // Split a version string into release and pre-release parts.
+            VersionParts(const std::string& ver) {
+                if (ver.empty())
+                    return;
 
-            str.erase(str.find_last_not_of(toTrim) + 1);
-        }
+                // Find first '+' and trim to it.
+                std::string version(ver.substr(0, ver.find('+')));
 
-        // Split a string on any of the given characters.
-        std::vector<std::string> split(const std::string& str,
-                                       const std::string& splitOn) {
-            std::vector<std::string> tokens;
-            size_t tokenStart = 0;
-            size_t tokenEnd = 0;
+                // Split release string.
+                size_t pos = version.find_first_of(" :_-");
+                std::string relStr(version.substr(0, pos));
+                trim(relStr, ".");
 
-            do {
-                tokenEnd = str.find_first_of(splitOn, tokenStart);
+                // Split release portion of version into what should be digit strings.
+                std::vector<std::string> temp(split(relStr, "."));
 
-                if (tokenEnd == std::string::npos) {
-                    tokens.push_back(str.substr(tokenStart));
+                if (!temp.empty()) {
+                    // Function to test if string does not contains only digits.
+                    auto notDigits = [](const std::string& str) {
+                        return !std::all_of(str.begin(), str.end(), ::isdigit);
+                    };
+
+                    // Unary string to unsigned long converter.
+                    auto toul = [](const std::string& str) {
+                        return std::stoul(str);
+                    };
+
+                    auto firstNonInt = std::find_if(temp.begin(), temp.end(), notDigits);
+
+                    std::transform(temp.begin(),
+                                   firstNonInt,
+                                   std::back_inserter(releaseNumbers),
+                                   toul);
+
+                    if (firstNonInt != temp.end())
+                        preReleaseStrings.assign(firstNonInt, temp.end());
                 }
-                else if (tokenEnd != tokenStart)
-                    tokens.push_back(str.substr(tokenStart, tokenEnd - tokenStart));
 
-                tokenStart = tokenEnd + 1;
-            } while (tokenEnd != std::string::npos);
+                // Now split the pre-release portion of the version string.
+                if (pos != std::string::npos) {
+                    std::string preRelStr(version.substr(pos));
+                    trim(preRelStr, ". :_-");
+                    temp = split(preRelStr, ". :_-");
 
-            return tokens;
-        }
-
-        // Split a version string into release and pre-release parts.
-        void decompose(const std::string& ver,
-                       std::vector<unsigned long>& release,
-                       std::vector<std::string>& preRelease) {
-            if (ver.empty())
-                return;
-
-            release.clear();
-            preRelease.clear();
-
-            // Find first '+' and trim to it.
-            std::string version(ver.substr(0, ver.find('+')));
-
-            // Split release string.
-            size_t pos = version.find_first_of(" :_-");
-            std::string relStr(version.substr(0, pos));
-            trim(relStr, ".");
-
-            // Split release portion of version into what should be digit strings.
-            std::vector<std::string> temp(split(relStr, "."));
-
-            if (!temp.empty()) {
-                // Function to test if string does not contains only digits.
-                auto notDigits = [](const std::string& str) {
-                    return !std::all_of(str.begin(), str.end(), ::isdigit);
-                };
-
-                // Unary string to unsigned long converter.
-                auto toul = [](const std::string& str) {
-                    return std::stoul(str);
-                };
-
-                auto firstNonInt = std::find_if(temp.begin(), temp.end(), notDigits);
-
-                std::transform(temp.begin(),
-                               firstNonInt,
-                               std::back_inserter(release),
-                               toul);
-
-                if (firstNonInt != temp.end())
-                    preRelease.assign(firstNonInt, temp.end());
+                    preReleaseStrings.insert(preReleaseStrings.end(),
+                                             temp.begin(),
+                                             temp.end());
+                }
             }
 
-            // Now split the pre-release portion of the version string.
-            if (pos != std::string::npos) {
-                std::string preRelStr(version.substr(pos));
-                trim(preRelStr, ". :_-");
-                temp = split(preRelStr, ". :_-");
+            int compare(VersionParts& other) {
+                // First compare release numbers.
+                int result = compareReleaseNumbers(other);
 
-                preRelease.insert(preRelease.end(), temp.begin(), temp.end());
+                if (result != 0)
+                    return result;
+
+                // Release strings are the same. Check pre-release strings.
+                return compareStrings(preReleaseStrings, other.preReleaseStrings);
             }
-        }
+
+        private:
+            std::vector<unsigned long> releaseNumbers;
+            std::vector<std::string> preReleaseStrings;
+
+            void normaliseReleaseNumbers(VersionParts& other) {
+                if (releaseNumbers.size() < other.releaseNumbers.size())
+                    releaseNumbers.resize(other.releaseNumbers.size(), 0);
+                else if (other.releaseNumbers.size() < releaseNumbers.size())
+                    other.releaseNumbers.resize(releaseNumbers.size(), 0);
+            }
+
+            int compareReleaseNumbers(VersionParts& other) {
+                normaliseReleaseNumbers(other);
+
+                for (size_t i = 0; i < releaseNumbers.size(); ++i) {
+                    if (releaseNumbers[i] < other.releaseNumbers[i])
+                        return -1;
+
+                    if (other.releaseNumbers[i] < releaseNumbers[i])
+                        return 1;
+                }
+
+                return 0;
+            }
+
+            static int compareStrings(const std::vector<std::string>& strings1,
+                                      const std::vector<std::string>& strings2) {
+                       // If one strings is empty while the other does not, the latter is less.
+                if (strings1.empty() != strings2.empty()) {
+                    if (!strings1.empty())
+                        return -1;
+                    else
+                        return 1;
+                }
+
+                if (strings1.empty())
+                    return 0;
+
+                auto isDigits = [](const std::string& str) {
+                    return std::all_of(str.begin(), str.end(), ::isdigit);
+                };
+
+                // Compare pre-release strings one by one.
+                size_t i = 0;
+                while (i < strings1.size() && i < strings2.size()) {
+                    bool v1IsInt = isDigits(strings1[i]);
+                    bool v2IsInt = isDigits(strings2[i]);
+
+                    // Integers have lower precedence than non-integer strings.
+                    if (v1IsInt != v2IsInt) {
+                        if (v1IsInt)
+                            return -1;
+                        else
+                            return 1;
+                    }
+
+                    if (v1IsInt) {
+                        // Compare integer values.
+                        unsigned long v1Int = std::stoul(strings1[i]);
+                        unsigned long v2Int = std::stoul(strings2[i]);
+
+                        if (v1Int < v2Int)
+                            return -1;
+                        if (v2Int < v1Int)
+                            return 1;
+                    }
+                    else {
+                        // Compare string values.
+                        if (strings1[i] < strings2[i])
+                            return -1;
+
+                        if (strings2[i] < strings1[i])
+                            return 1;
+                    }
+
+                    ++i;
+                }
+
+                // Have reached the end of one or both pre-release string vectors.
+                // If only the end of one was reached, it is less.
+                if (strings1.size() == strings2.size())
+                    return 0;
+                else if (i == strings1.size())
+                    return -1;
+                else
+                    return 1;
+            }
+
+            // Trim start and end of a string, removing any of the given characters.
+            static void trim(std::string& str, const std::string& toTrim) {
+                size_t pos = str.find_first_not_of(toTrim);
+                if (pos != 0)
+                    str.erase(0, str.find_first_not_of(toTrim));
+
+                str.erase(str.find_last_not_of(toTrim) + 1);
+            }
+
+            // Split a string on any of the given characters.
+            static std::vector<std::string> split(const std::string& str,
+                                                  const std::string& splitOn) {
+                std::vector<std::string> tokens;
+                size_t tokenStart = 0;
+                size_t tokenEnd = 0;
+
+                do {
+                    tokenEnd = str.find_first_of(splitOn, tokenStart);
+
+                    if (tokenEnd == std::string::npos) {
+                        tokens.push_back(str.substr(tokenStart));
+                    }
+                    else if (tokenEnd != tokenStart)
+                        tokens.push_back(str.substr(tokenStart, tokenEnd - tokenStart));
+
+                    tokenStart = tokenEnd + 1;
+                } while (tokenEnd != std::string::npos);
+
+                return tokens;
+            }
+        };
     }
 
     int compare(const std::string& ver1, const std::string ver2) {
@@ -108,87 +206,10 @@ namespace pseudosem {
         version or metadata) to equal length before comparison.
         */
 
-        std::vector<unsigned long> v1Release, v2Release;
-        std::vector<std::string> v1PreRelease, v2PreRelease;
+        detail::VersionParts v1(ver1);
+        detail::VersionParts v2(ver2);
 
-        detail::decompose(ver1, v1Release, v1PreRelease);
-        detail::decompose(ver2, v2Release, v2PreRelease);
-
-        // Compare lengths of release vectors, and pad shorter with zeroes.
-        if (v1Release.size() < v2Release.size())
-            v1Release.resize(v2Release.size(), 0);
-        else if (v2Release.size() < v1Release.size())
-            v2Release.resize(v1Release.size(), 0);
-
-        // Now compare release numbers one by one.
-        for (size_t i = 0; i < v1Release.size(); ++i) {
-            if (v1Release[i] < v2Release[i])
-                return -1;
-
-            if (v2Release[i] < v1Release[i])
-                return 1;
-        }
-
-        // Release numbers are the same. Check pre-release strings. If one has
-        // pre-release strings while the other does not, the first is less.
-        if (v1PreRelease.empty() != v2PreRelease.empty()) {
-            if (!v1PreRelease.empty())
-                return -1;
-            else
-                return 1;
-        }
-
-        if (v1PreRelease.empty())
-            return 0;
-
-        auto isDigits = [](const std::string& str) {
-            return std::all_of(str.begin(), str.end(), ::isdigit);
-        };
-
-        // Compare pre-release strings one by one.
-        size_t i = 0;
-        while (i < v1PreRelease.size() && i < v2PreRelease.size()) {
-            bool v1IsInt = isDigits(v1PreRelease[i]);
-            bool v2IsInt = isDigits(v2PreRelease[i]);
-
-            // Integers have lower precedence than non-integer strings.
-            if (v1IsInt != v2IsInt) {
-                if (v1IsInt)
-                    return -1;
-                else
-                    return 1;
-            }
-
-            if (v1IsInt) {
-                // Compare integer values.
-                unsigned long v1Int = std::stoul(v1PreRelease[i]);
-                unsigned long v2Int = std::stoul(v2PreRelease[i]);
-
-                if (v1Int < v2Int)
-                    return -1;
-                if (v2Int < v1Int)
-                    return 1;
-            }
-            else {
-                // Compare string values.
-                if (v1PreRelease[i] < v2PreRelease[i])
-                    return -1;
-
-                if (v2PreRelease[i] < v1PreRelease[i])
-                    return 1;
-            }
-
-            ++i;
-        }
-
-        // Have reached the end of one or both pre-release string vectors.
-        // If only the end of one was reached, it is less.
-        if (v1PreRelease.size() == v2PreRelease.size())
-            return 0;
-        else if (i == v1PreRelease.size())
-            return -1;
-        else
-            return 1;
+        return v1.compare(v2);
     }
 }
 
